5f09c70c3ed4d89bad09e550496392de668450c1
tlduf95 <tlduf95@naver.com>
2020-03-24 18:13:41 +0900

    Updates

---
+++ precache-manifest.9702ad6f434ce0fe03a63587276202ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954707be44a60d04ed7f9cfb648ea713",
+    "url": "/site_app_2020/index.html"
+  },
+  {
+    "revision": "77cf44b28ee803102f1f",
+    "url": "/site_app_2020/static/css/main.0c7338a8.chunk.css"
+  },
+  {
+    "revision": "cb43fa7cde42e4fb5f59",
+    "url": "/site_app_2020/static/js/2.7df552b8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/site_app_2020/static/js/2.7df552b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77cf44b28ee803102f1f",
+    "url": "/site_app_2020/static/js/main.4643f920.chunk.js"
+  },
+  {
+    "revision": "0825eea0bfe10afeb4bc",
+    "url": "/site_app_2020/static/js/runtime-main.cc0564cd.js"
+  }
+]);
